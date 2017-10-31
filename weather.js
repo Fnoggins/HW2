2cb4bf77539e5e18d6ec15626cc1de22e36e2fa6
Fnoggins <kelfong@outlook.com>
2017-10-31 12:14:48 -0500

    HW2 2017-10-31-1214

--- weather.js
+++ weather.js
@@ -1,58 +1,45 @@
 let convertToJSON = function(response) {
-  console.log(response);
   return response.json();
 }
 
 let updateWeather = function(dataFromService) {
-  console.log(json);
-  return json;
-
+  console.log(dataFromService);
   let city = dataFromService.name;
   let temperature = dataFromService.main.temp;
-  // To get the relevant weather icon from the openweathermap API:
-  // let icon = dataFromService.weather[0].icon;
+  let icon = dataFromService.weather[0].icon;
 
-  document.getElementByClass("card-title")
-    for(var i = 0; i < x.length; i++){
-      x[i].innerText=city;
-  document.getElementByClass("card-text")
-    for(var i = 0; i < x.length; i++){
-      x[i].innerText="It is "+temperature+" degrees outside.";
+  let cardTitleElement = document.getElementsByClassName("card-title")[0];
+  cardTitleElement.innerText=city;
+  let cardTextElement = document.getElementsByClassName("card-text")[0];
+  cardTextElement.innerText="It is "+temperature.toFixed(0)+" degrees outside.";
+  let cardIconElement = document.getElementsByClassName("card-img-top")[0];
+  cardIconElement.src = "http://openweathermap.org/img/w/"+icon+".png";
 }
 
-let getWeather = function(lat, lon) {
-  let latitude = '41.8781';
-  let longitude = '-87.6298';
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+}
 
-  // SCRIPT FOR RETURNING LATITUDE + LONGITUDE
-  //
-  // <script>
-  // var x = document.getElementById("demo");
-  // function getLocation() {
-  //     if (navigator.geolocation) {
-  //         navigator.geolocation.getCurrentPosition(showPosition);
-  //     } else {
-  //         x.innerHTML = "Geolocation is not supported by this browser.";
-  //     }
-  // }
-  // function showPosition(position) {
-  //     x.innerHTML = "Latitude: " + position.coords.latitude +
-  //     "<br>Longitude: " + position.coords.longitude;
-  // }
-  // </script>
-
+function showPosition(position) {
+  console.log(position)
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
   let openweathermap_api_url = 'https://api.openweathermap.org/data/2.5/weather?'
   openweathermap_api_url += 'lat=' + latitude
   openweathermap_api_url += '&lon=' + longitude
   openweathermap_api_url +='&appid=4ce6f502d38ddae567bf1702b05e168c&units=imperial'
+  fetch(openweathermap_api_url).then(convertToJSON).then(updateWeather);
+}
 
-  fetch(openweathermap_api_url).then(convertToJSON).then(updateWeather).catch(displayError);
-
-  document.getElementById("get_forecast").addEventListener("click", getWeather);
-
+let getWeather = function(lat, lon) {
+  getLocation();
 }
 
 let displayError = function(error) {
   console.debug(error);
   window.alert("Sorry, something went wrong.");
 }
+
+document.getElementById("get_forecast").addEventListener("click", getWeather);
